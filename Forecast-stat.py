7144f98c4ef15451b81b6c5ef80c5957df4483b3
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-12-22 14:39:19 +0700

    Update Forecast-stat.py

--- Forecast-stat.py
+++ Forecast-stat.py
@@ -7,6 +7,7 @@ Created on Sat Aug 20 10:25:06 2022
 
 import itertools
 from prophet import Prophet
+from sklearn.metrics import mean_squared_error as MSE
 import pmdarima as pmd
 import pandas as pd 
 import numpy as np
@@ -16,18 +17,31 @@ from scipy.stats import zscore
 import statsmodels.api as sm
 #import statsmodels.formula.api as smf
 #import datetime as dt
+from sklearn.metrics import mean_squared_error
+import os
+username = os.getlogin()
 
-df = pd.DataFrame(pd.read_excel(r'C:\Users\PhamGiaPhu\OneDrive - DuyTan Plastics\Script\AS - Prophet.xlsx',index_col=0))
+
+
+df = pd.DataFrame(pd.read_excel(r'C:\Users\{}\OneDrive - DuyTan Plastics\BI Planning\AS - FC.xlsx'.format(username),sheet_name = "B2C",index_col=0))
 #df = pd.DataFrame(pd.read_excel(r'C:\Users\PhamGiaPhu\OneDrive - DuyTan Plastics\Script\ASB2Bbtm.xlsx',index_col=0))
 #df = pd.DataFrame(pd.read_excel(r'C:\Users\PhamGiaPhu\OneDrive - DuyTan Plastics\Script\AS - B2C Month aT.xlsx',index_col=0))
 
 #fill nan with median of to prepare data for outlier detection
 df = df.fillna(df.median())
-df = df[(np.abs(df.apply(zscore))<2.3)]
+df = df[(np.abs(df.apply(zscore))<2.5)]
 df = df.fillna(df.median())
 wd = pd.read_excel(r'C:\Users\PhamGiaPhu\OneDrive - DuyTan Plastics\Master Data\WD.xlsx',sheet_name = "Sheet1",index_col=0)
+
+tempe = pd.read_excel(r'C:\Users\PhamGiaPhu\OneDrive - DuyTan Plastics\Master Data\Temperature.xlsx',sheet_name = "Sheet1",index_col=0)
+
+
+
 #forecast period
 fcperiod = 12
+
+
+
 
 
 #create list of forecast date
@@ -49,23 +63,26 @@ df_P = pd.DataFrame()
 df_HW = pd.DataFrame()
 df_UCM = pd.DataFrame()
 df_SARIMA = pd.DataFrame()
-df_param = pd.DataFrame(data={'Model': ['SES','Holt-Winter','SARIMAX','UCM']})
-
+df_best = pd.DataFrame()
+df_param = pd.DataFrame(data={'Model': ['Prophet','SES','Holt-Winter','SARIMAX','UCM']})
+df_rmse = pd.DataFrame(data={'Model': ['Prophet','SES','Holt-Winter','SARIMAX','UCM']})
 
 for sku in list(df):
     try:
         #Prophet
+
         param_gridsearch = {  
             'changepoint_prior_scale': [0.1, 0.5],
-            'growth': ['logistic','linear','flat'],
-            'seasonality_prior_scale': [1.0, 10.0],
+            'growth': ['logistic','linear'],
+            #'seasonality_prior_scale': [0.1, 4],
             'seasonality_mode': ['additive', 'multiplicative'],
-            'n_changepoints': [3,6],
-            'changepoint_range': [0.95],
+            'n_changepoints': [3],
                             }
+        
+        
         all_params = [dict(zip(param_gridsearch.keys(), v)) for v in itertools.product(*param_gridsearch.values())]
         rmses = []
-        cutoffs = pd.date_range(start='2022-05-01', end='2022-07-01', freq='MS') 
+
         
         
         df_model = pd.DataFrame(df[sku].copy()).reset_index()
@@ -74,15 +91,24 @@ for sku in list(df):
         df_model['floor'] = df_model.y.quantile(0.1)
         df_model['wd'] = np.asarray(exog_fit)
          
+        for params in all_params:
+            #cross validation search for best fit
+            m = (
+                Prophet(**params,weekly_seasonality=False,daily_seasonality=False,yearly_seasonality=False,uncertainty_samples=0)
+                        .add_seasonality(name='monthly', period=12, fourier_order=12,prior_scale=0.1)
+                        .add_regressor('wd')
+                        .add_country_holidays(country_name='VN')
+                        .fit(df_model))
+
+            rmses.append(np.sqrt(MSE(df_model['y'], m.predict(df_model)['yhat'] )))
+            
+        best_params = all_params[np.argmin(rmses)]   
         m = (
-            Prophet(growth='logistic',
-                    changepoint_prior_scale=0.2,uncertainty_samples=0,
-                    weekly_seasonality=False,daily_seasonality=False,
-                    seasonality_mode='additive')
-                    .add_seasonality(name='monthly', period=12, 
-                                     fourier_order=12,prior_scale=0.4).
-                    add_regressor('wd')
-                    .fit(df_model))
+            Prophet(**best_params,weekly_seasonality=False,daily_seasonality=False,yearly_seasonality=False,uncertainty_samples=0)
+                    .add_seasonality(name='monthly', period=12, fourier_order=12,prior_scale=0.1)
+                    .add_regressor('wd')
+                    .add_country_holidays(country_name='VN')
+                    .fit(df_model))  
         if sku == firstsku:
             df_f = m.make_future_dataframe(periods=fcperiod,freq='MS')
         else:
@@ -93,18 +119,22 @@ for sku in list(df):
         forecast = m.predict(df_f)
         df_P[sku] = forecast.yhat.tail(fcperiod)
         #fig = m.plot(forecast)
+        df_param[sku] = np.nan
+        df_rmse[sku] = np.nan
+        df_param[sku] = df_param[sku].astype('object')
+        df_rmse[sku] = df_param[sku].astype('object')
+        df_param.at[df_param[df_param['Model'] == 'Prophet'].index[0],sku] = ( best_params  )
+        df_rmse.at[df_rmse[df_rmse['Model'] == 'Prophet'].index[0],sku]  = np.sqrt(MSE(df_model['y'], m.predict(df_model)['yhat'] ))
     
         #SES model    
         fitSES = sm.tsa.SimpleExpSmoothing(np.asarray(df[sku])).fit(optimized=True)
-        arr_forecast = fitSES.forecast(fcperiod)
-        df_SES[sku] = arr_forecast
-        df_param[sku] = np.nan
-        df_param[sku] = df_param[sku].astype('object')
+        df_SES[sku] = fitSES.forecast(fcperiod)
         df_param.at[df_param[df_param['Model'] == 'SES'].index[0],sku] = ( fitSES.params.get('smoothing_level')  )
+        df_rmse.at[df_rmse[df_rmse['Model'] == 'SES'].index[0],sku] = mean_squared_error(np.asarray(df[sku]), fitSES.fittedvalues, squared=False)
     
         #Holt-Winter model
         HW_param_gridsearch = {  
-            'initialization_method': ['heuristic','estimated','legacy-heuristic'],
+            'initialization_method': ['heuristic','estimated'],
             'seasonal': ['add','mul'],
             'trend': ['add','mul'],
             'damped_trend': [True,False],
@@ -118,9 +148,9 @@ for sku in list(df):
         fitHW = sm.tsa.ExponentialSmoothing(np.asarray(df[sku]), seasonal_periods=12,**minhw).fit(optimized=True)
        #fitHW = sm.tsa.ExponentialSmoothing(np.asarray(df[sku]), initialization_method='heuristic',seasonal_periods=12,trend='add', seasonal='add',damped_trend=True).fit(optimized=True)
  
-        arr_forecast = fitHW.forecast(fcperiod)
-        df_HW[sku] = arr_forecast 
-        df_param.at[df_param[df_param['Model'] == 'Holt-Winter'].index[0],sku] = ( minhw.update(fitHW.params)  )
+        df_HW[sku] = fitHW.forecast(fcperiod)
+        df_param.at[df_param[df_param['Model'] == 'Holt-Winter'].index[0],sku] = ( minhw )
+        df_rmse.at[df_rmse[df_rmse['Model'] == 'Holt-Winter'].index[0],sku] = mean_squared_error(np.asarray(df[sku]), fitHW.fittedvalues, squared=False)
         
         
         
@@ -130,22 +160,20 @@ for sku in list(df):
     
         #SARIMA  
         #check for how many diffs we need
-        d = pmd.arima.ndiffs(np.asarray(df[sku])) #first diff
-        D = pmd.arima.nsdiffs(np.asarray(df[sku]), 12) #seasonal diff
         ap_autoarimamodel = pmd.arima.auto_arima(np.asarray(df[sku]),
                                                  information_criterion = 'aicc',
                                                  exogenous=exog_fit,
-                                                 start_p=0, max_p=12,
-                                                 d=d, max_d=d,
-                                                 start_q=0, max_q=12,
+                                                 start_p=0, max_p=5,
+                                                 d=1, max_d=1,
+                                                 start_q=0, max_q=5,
                                                  start_P=0, max_P=3,
                                                  start_Q=0, max_Q=3,
-                                                 D=D,max_D=D,
+                                                 D=1,max_D=1,
                                                  m=12,seasonal=True,
-                                                 trace=False,supress_warnings=True,stepwise=True,error_action='ignore',random_state=20,n_fits=50)
+                                                 trace=False,supress_warnings=True,stepwise=True,with_intercept=True,error_action='ignore',random_state=25,n_fits=50)
 
         df_param.at[df_param[df_param['Model'] == 'SARIMAX'].index[0],sku] = ap_autoarimamodel.get_params().get("order") + ap_autoarimamodel.get_params().get("seasonal_order")
-        
+        df_rmse.at[df_rmse[df_rmse['Model'] == 'SARIMAX'].index[0],sku] = mean_squared_error(np.asarray(df[sku]), ap_autoarimamodel.arima_res_.fittedvalues, squared=False)
         try:
            # arr_forecast = ap_autoarimamodel.predict(n_periods=fcperiod,
            #                                          exogenous=exog_fc,
@@ -160,36 +188,57 @@ for sku in list(df):
         #UCM model
         #grid search
         UCM_param_gridsearch = {  
-            'level': ['ntrend','dconstant','llevel','rwalk','dtrend','lldtrend','rwdrift','lltrend','strend','rtrend'],
+            #'level': ['ntrend','dconstant','llevel','rwalk','dtrend','lldtrend','rwdrift','lltrend','strend','rtrend'],
+            'level': ['ntrend','lldtrend','lltrend','strend','rtrend'],
             'cycle': [True,False],
             'irregular': [True,False],
             'damped_cycle': [True,False],
             'use_exact_diffuse': [True,False],
-            'autoregressive': [1,2]
+            'autoregressive': [0,1]
                             }
         UCM_all_params = [dict(zip(UCM_param_gridsearch.keys(), v)) for v in itertools.product(*UCM_param_gridsearch.values())]
         aicc =[]
         for params in UCM_all_params: 
-               aicc.append(
-                   sm.tsa.UnobservedComponents(
-                   np.asarray(df[sku]),
-                   exog = exog_fit,
-                   **params,
-                   freq_seasonal=[{'period':12,'harmonics':12}]).fit().aicc)
+               try:
+                   aicc.append(
+                       sm.tsa.UnobservedComponents(
+                       np.asarray(df[sku]),
+                       exog = exog_fit,
+                       **params,
+                       freq_seasonal=[{'period':12,'harmonics':12}]).fit().aicc)
+               except:
+                   aicc.append(
+                      sm.tsa.UnobservedComponents(
+                      np.asarray(df[sku]),
+                      exog = exog_fit,
+                      **params,
+                      freq_seasonal=[{'period':12,'harmonics':12}]).fit(method='cg').aicc)
+        
         minaicc = UCM_all_params[np.argmin(aicc)]
 
         fitUCM = sm.tsa.UnobservedComponents(
             np.asarray(df[sku]),
             exog = exog_fit,
-            level= minaicc,
-            cycle=True,irregular=True,damped_cycle=True,
+            **minaicc,
             freq_seasonal=[{'period':12,'harmonics':12}]).fit()
-        arr_forecast = fitUCM.forecast(fcperiod,exog = exog_fc)
-        df_UCM[sku] = arr_forecast
-        df_param.at[df_param[df_param['Model'] == 'UCM'].index[0],sku] = ( minaicc  )
+        df_UCM[sku] = fitUCM.forecast(fcperiod,exog = exog_fc)
+        df_param.at[df_param[df_param['Model'] == 'UCM'].index[0],sku] = ( minaicc )
         #df_UCM.set_index(future_index,inplace=True)
+        df_rmse.at[df_rmse[df_rmse['Model'] == 'UCM'].index[0],sku] = mean_squared_error(np.asarray(df[sku]), fitUCM.fittedvalues, squared=False)
     except:
         pass
+    
+    if df_rmse['Model'][np.argmin(df_rmse[sku])] == 'Prophet':
+        df_best[sku] = df_P[sku]
+    elif df_rmse['Model'][np.argmin(df_rmse[sku])] == 'SES':
+        df_best[sku] = df_SES[sku]
+    elif df_rmse['Model'][np.argmin(df_rmse[sku])] == 'Holt-Winter':
+        df_best[sku] = df_HW[sku]
+    elif df_rmse['Model'][np.argmin(df_rmse[sku])] == 'SARIMAX':
+        df_best[sku] = df_SARIMA[sku]
+    else:
+        df_best[sku] = df_UCM[sku]
+
 
 df_P.set_index(future_index,inplace=True)
 df_P['Model'] = 'Prophet'
@@ -202,13 +251,22 @@ df_UCM['Model'] = 'UCM'
 df_SARIMA.set_index(future_index,inplace=True)
 df_SARIMA['Model'] = 'SARIMA'
 
-#comparing model and select best fit
-aiccmodel = {'SES': fitSES.aicc,
-             'Holt-Winter': fitHW.aicc,
-             'SARIMAX': ap_autoarimamodel.aicc(),
-             'UCM': fitUCM.aicc
-             }
 
 
-pd.concat([df_SES,df_P,df_HW,df_UCM,df_SARIMA],axis=0).to_excel('FC B2C Weekly Adjust.xlsx')
-df_param.to_excel('Param.xlsx')
+
+pd.concat([df_SES,df_P,df_HW,df_UCM,df_SARIMA],axis=0).to_excel(r'C:\Users\{}\Downloads\StatFC_Full.xlsx'.format(username))
+df_param.to_excel(r'C:\Users\{}\Downloads\StatFC_Params.xlsx'.format(username))
+df_rmse.to_excel(r'C:\Users\{}\Downloads\StatFC_RMSE.xlsx'.format(username))
+df_best.to_excel(r'C:\Users\{}\Downloads\StatFC.xlsx'.format(username))
+
+
+
+
+#plot
+
+#fig, ax = plt.subplots()
+#ax.plot(df[sku], color='k', label='data')
+#ax.plot(df_UCM[sku], label='predicted')
+
+
+
